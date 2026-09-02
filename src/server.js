8f9812fa91d97a9b6d02b72c3de94e69336d2f2c
Rohit_chaudhary <rchaudhary4758@gmail.com>
2026-09-02 20:14:09 +0530

    utils code Discation

--- src/server.js
+++ src/server.js
@@ -5,6 +5,7 @@
 // dns.setServers(["8.8.8.8","1.1.1.1"])
 
 import './utils/dnsSetup.js'; 
+import app from "./app.js";
 
 import dotenv, { config } from "dotenv";
 import connectDB from "./db/database.db.js";
@@ -17,3 +18,12 @@ dotenv.config({
 
 
 connectDB()
+.then(() => {
+    app.listen(process.env.PORT || 3000, () => {
+        console.log(`Server is running on port ${process.env.PORT || 3000}`);
+    })
+})
+.catch((err) => {
+    console.error("Error connecting to the database:", err);
+    process.exit(1); // Exit the process with failure
+  });
---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,54 @@
+    const  asyncHandler = (requestHandler) =>  {
+
+      return  (req,res,next) => { Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err))
+
+       }
+
+    }
+
+
+
+
+
+export {asyncHandler}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const asyncHandler = (fn) => async(req, res, next) => {
+//     try {
+        
+//     } catch (error) {
+//         await fn(req, res, next)
+
+//         res.status(err.code || 500).json({
+//             success : false,
+//             message:err.message
+
+//         })
+        
+//     }
+// }
